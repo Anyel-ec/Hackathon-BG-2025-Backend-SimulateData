8e5a74464ae74602effd7913bb3fcfbc524f1a3e
anyel-ec <paulangel826@gmail.com>
2025-03-08 08:50:44 -0500

    chore: initial project in spring boot 3.4.3

---
+++ src/test/java/com/guayaquil/hackathon/HackathonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guayaquil.hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
