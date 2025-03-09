b4802e4d0e523da8d25ba6e935b6130ccb8d1f72
anyel-ec <paulangel826@gmail.com>
2025-03-09 13:17:15 -0500

    feat: update jar - anyelec

--- src/main/java/com/guayaquil/hackathon/HackathonBackendApplication.java
+++ src/main/java/com/guayaquil/hackathon/HackathonBackendApplication.java
@@ -1,6 +1,5 @@
 package com.guayaquil.hackathon;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,13 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class HackathonBackendApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().load();
-
-        // Set environment variables as system properties so Spring can access them
-        System.setProperty("POSTGRES_URL", dotenv.get("POSTGRES_URL", ""));
-        System.setProperty("POSTGRES_USERNAME", dotenv.get("POSTGRES_USERNAME", ""));
-        System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD", ""));
-
         SpringApplication.run(HackathonBackendApplication.class, args);
     }
 
